8bbd980e4bfaaf7b6736829bdf39c6059d559284
edwarjuliangraciabernate-pixel <edwarjuliangraciabernate@gmail.com>
2026-06-03 22:50:15 -0500

    Resolver conflicto de merge en el Dashboard (estructura y layout principal)

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,12 @@
 import Navbar from "../components/layout/Navbar";
 import Sidebar from "../components/layout/Sidebar";
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 >>>>>>> 009dea9 (Primer Commit del Proyecto)
+=======
+>>>>>>> 7f376fa (Resolver conflicto de merge en el Dashboard (estructura y layout principal))
 import Card from "../components/ui/Card";
 
 function Dashboard() {
@@ -11,27 +14,12 @@ function Dashboard() {
     <div>
       <Navbar />
 
-<<<<<<< HEAD
-      <div style={{ display: 'flex' }}>
+      <div style={{ display: "flex" }}>
         <Sidebar />
 
-        <main style={{ padding: '80px', flex: 1, overflow: 'hidden' }}>
-          <div style={{ color: "#1E3A8A" }}></div>
-          <h6>--BIENVENIDO/A-- </h6>
-          <h1>¡Bienvenido a Buydrax!</h1> 
-           <Card title="Ventas Totales"><p>$15,000</p></Card>
-            <Card title="Usuarios Activos"><p>250</p></Card>
-            <Card title="Pedidos"><p>1,200</p></Card>
-          <div style={{ display: 'flex', gap: '100px', marginTop: '250px' }}>
-          </div>
-        </main>
-
-=======
-      <div className="container">
-        <Sidebar />
-
-        <main>
-          <h1>Dashboard</h1>
+        <main style={{ padding: "80px", flex: 1 }}>
+          <h6>--BIENVENIDO/A--</h6>
+          <h1>¡Bienvenido a Buydrax!</h1>
 
           <Card title="Ventas Totales">
             <p>$15,000</p>
@@ -45,10 +33,14 @@ function Dashboard() {
             <p>1,200</p>
           </Card>
         </main>
+<<<<<<< HEAD
 >>>>>>> 009dea9 (Primer Commit del Proyecto)
+=======
+>>>>>>> 7f376fa (Resolver conflicto de merge en el Dashboard (estructura y layout principal))
       </div>
     </div>
   );
 }
 
+export default Dashboard;
 export default Dashboard;
