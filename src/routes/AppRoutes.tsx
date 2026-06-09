87ce47fb45bd83059b222f9384407e90fa43970b
YerikHerrera <yerikcastanuela@gmail.com>
2026-06-09 18:28:47 -0500

    feat(empleados): Agregar página Lista de Empleados con filtros, paginación y acciones

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,21 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AgregarEmpleado from "../pages/empleados";
-
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
 import Users from "../pages/Users";
-import Empleados from "../pages/AgregarEmpleados";
-
+import AgregarEmpleados from "../pages/AgregarEmpleados";
+import ListaEmpleados from "../pages/ListaEmpleados";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/empleados/agregar" element={<AgregarEmpleado />} />
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/users" element={<Users />} />
-        <Route path="/empleados" element={<Empleados />} />
+        <Route path="/empleados" element={<ListaEmpleados />} />
+        <Route path="/empleados/agregar" element={<AgregarEmpleado />} />
+        <Route path="/empleados/lista" element={<ListaEmpleados />} />
+        <Route path="/empleados/registro" element={<AgregarEmpleados />} />
       </Routes>
     </BrowserRouter>
   );
