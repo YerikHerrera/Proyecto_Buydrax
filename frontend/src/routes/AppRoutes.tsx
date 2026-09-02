e4ce5c154b255e2870e99117e1e84412d157dfa5
edwarjuliangraciabernate-pixel <edwarjuliangraciabernate@gmail.com>
2026-09-02 16:58:56 -0500

    se creo el apartado de reportes, con su tabla con ELIMINAR y EDITAR

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
 import Users from "../pages/Users";
-import AgregarEmpleados from "../pages/AgregarEmpleados";  // solo este
+import AgregarEmpleados from "../pages/AgregarEmpleados";
 import CrearProyecto from "../pages/CrearProyecto";
 import ListaEmpleados from "../pages/ListaEmpleados";
 import AsignacionEmpleado from "../pages/AsignacionEmpleado";
@@ -10,22 +11,50 @@ import Supervisor from "../pages/Supervisor";
 import Asistencia from "../pages/asistencia";
 import HorasExtras from "../pages/HorasExtras";
 
-<><Route path="/proyectos" element={<CrearProyecto />} /><Route path="/proyectos/asignacion" element={<AsignacionEmpleado />} /><Route path="/proyectos/supervisor" element={<Supervisor />} /></>
- 
-/*Estas son las rutas que se utilizarán en la aplicación, define como se localiza cada elemento*/ 
+// NUEVO: página de reportes
+import Reportes from "../pages/Reportes";
+
+/* Estas son las rutas que se utilizarán en la aplicación */
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<Login />} />
+
         <Route path="/dashboard" element={<Dashboard />} />
+
         <Route path="/users" element={<Users />} />
+
+        {/* EMPLEADOS */}
         <Route path="/empleados" element={<ListaEmpleados />} />
         <Route path="/empleados/lista" element={<ListaEmpleados />} />
-        <Route path="/empleados/agregar" element={<AgregarEmpleados />} />  {/* ← formulario real */}
+        <Route
+          path="/empleados/agregar"
+          element={<AgregarEmpleados />}
+        />
+
+        {/* PROYECTOS */}
         <Route path="/proyectos" element={<CrearProyecto />} />
+        <Route
+          path="/proyectos/asignacion"
+          element={<AsignacionEmpleado />}
+        />
+        <Route
+          path="/proyectos/supervisor"
+          element={<Supervisor />}
+        />
+
+        {/* ASISTENCIA */}
         <Route path="/asistencia" element={<Asistencia />} />
-        <Route path="/asistencia/reporte" element={<HorasExtras />} />
+        <Route
+          path="/asistencia/reporte"
+          element={<HorasExtras />}
+        />
+
+        {/* REPORTES */}
+        <Route path="/reportes" element={<Reportes />} />
+
       </Routes>
     </BrowserRouter>
   );
